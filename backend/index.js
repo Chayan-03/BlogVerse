c74ebfc5a4f56f99733e8faba780e6c287533896
Chayan-03 <chayan.06014802722@cse.mait.ac.in>
2025-05-11 14:46:20 +0530

    Routes Added

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  origin: "https://blogverse-eight.vercel.app/", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
